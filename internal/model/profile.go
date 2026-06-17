c9cb3e8c02246d4815e10b9844d2fc9332e7be03
y08416 <kesuyo27@icloud.com>
2026-06-18 01:03:28 +0900

    feat: プロフィール取得時にavatar_urlを返す

--- internal/model/profile.go
+++ internal/model/profile.go
@@ -1,10 +1,10 @@
 package model
 
 type Profile struct {
-	ID        string `json:"id"`
-	Name      string `json:"name"`
-	AvatarURL string `json:"avatar_url"`
-	Gender    string `json:"gender"`
+	ID        string  `json:"id"`
+	Name      string  `json:"name"`
+	AvatarURL *string `json:"avatar_url"`
+	Gender    string  `json:"gender"`
 }
 
 type UpdateProfileRequest struct {
