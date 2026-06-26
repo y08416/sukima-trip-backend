e64e69545231f31af3da48bab4a911c0f237da73
y08416 <kesuyo27@icloud.com>
2026-06-27 06:17:08 +0900

    fix: GET /favorites を spot_likes から取得しいいね=お気に入りに統一する

--- internal/model/favorite.go
+++ internal/model/favorite.go
@@ -1,14 +1,12 @@
 package model
 
 type Favorite struct {
-	ID         string  `json:"id"`
-	UserID     string  `json:"user_id"`
-	PlaceID    string  `json:"place_id"`
-	Name       string  `json:"name"`
-	Latitude   float64 `json:"latitude"`
-	Longitude  float64 `json:"longitude"`
-	CreatedAt  string  `json:"created_at"`
-	CoinAmount int     `json:"coin_amount"`
+	ID         string `json:"id"`
+	UserID     string `json:"user_id"`
+	PlaceID    string `json:"place_id"`
+	Name       string `json:"name"`
+	CreatedAt  string `json:"created_at"`
+	CoinAmount int    `json:"coin_amount"`
 }
 
 type SaveFavoriteRequest struct {
